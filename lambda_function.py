325b9fa3b2af721cbf2e954f3f01895b5d5dc173
Brian Kelley <bkelley620@gmail.com>
2016-08-07 11:52:21 -0700

    Made it so the bot will post to morebottesting, general cleanup

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,8 @@ import json
 
 import secrets
 
+import requests
+
 ENCRYPTED_EXPECTED_TOKENS = secrets.token
 
 kms = boto3.client('kms')
@@ -18,33 +20,73 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('RRS_Privileged_Users')
-privileged_users = [str(user['username']) for user in table.scan()['Items']]
+users_table = dynamodb.Table('RRS_Privileged_Users')
 
-post_channels = ['morebottesting', 'bottesting']
+post_channel = '#morebottesting'
+parse_level = 'full'
+username = 'Watney'
+icon_url = 'https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2016-08-01/65268633191_44503e401be992822065_48.jpg'
 
 ## Usernames must start with a letter or number, but then can contain periods, dashes, 
 ## and underscores. All lowercase.
 username_regex = r'^[a-z0-9][a-z0-9._-]*$'
 
+post_message_url = 'https://slack.com/api/chat.postMessage'
 
-def post(user, command_text, channel):
+def privileged_users():
+    return [str(user['username']) for user in users_table.scan()['Items']]
+
+def construct_json(**kwargs):
+    '''
+    Given keyword arguments, construct a JSON string based on that.
+
+    Example:
+    
+        construct_json(text='\'Hello\', "world"') 
+    
+    should return 
+
+        {
+            "text": "'Hello', \"world\""
+        }
+    '''
+    return json.dumps(kwargs)
+
+def post_to_channel(message):
+    data = {
+        'text': message, 
+        'token': secrets.slack_auth_token,
+        'channel': post_channel,
+        'parse_level': parse_level,
+        'username': username,
+        'icon_url': icon_url
+    }
+
+    return requests.post(post_message_url, data=data)
+
+
+def post(user, command_text):
     '''
     Given a message, post that to #general and email it to the email list if 
-    the user running the command has those priviliges
+    the user running the command has those privileges
 
     Returns an error message if not successful, otherwise a success message
     '''
-    if user not in privileged_users:
-        # logger.warn('Unprivileged user %s attempted to make announcement %s to the members' % 
-        #         (user, command_text))
-        return '{"channel" : "%s", "text" : "You are not a privileged user. This attempt has been logged."}' %channel
+    if user not in privileged_users():
+        logger.warn('Unprivileged user %s attempted to make announcement %s to the members' % 
+                (user, command_text))
+        return construct_json(text='You are not a privileged user. This attempt has been logged.')
         ## TODO: log this attempt to DynamoDB, potentially use SNS to notify officers?
 
-    ## TODO: Post message in channel, email to mailing list.
-    return ('{"response_type": "in_channel", "channel" : "%s",  "text" : "User %s has posted the following message : \'%s\'. '
-            'It has been posted in general and emailed to the mailing list."}') % (channel, user, command_text)
+    response = post_to_channel(command_text)
 
+    if response.status_code != 200:
+        return construct_json(text='Something went wrong posting the message! Got %d %d' 
+            % (response.status_code, response.reason))
+
+    return construct_json(text=('Successfully posted the following message: "%s". '
+        'It has been posted in %s and emailed to the mailing list'
+        % (command_text, post_channel)))
 
 def add_privileged_user(user, command_text):
     '''
@@ -52,16 +94,16 @@ def add_privileged_user(user, command_text):
 
     Returns an error message if not successful, otherwise a success message
     '''
-    if user not in privileged_users:
+    if user not in privileged_users():
         logger.warn('Unprivileged user %s ran /adduser on %s' % 
                 (user, command_text))
-        return 'You are not a privileged user. This attempt has been logged.'
+        return construct_json(text='You are not a privileged user. This attempt has been logged.')
         ## TODO: log this attempt to DynamoDB, potentially use SNS to notify officers?
 
     cleaned_name = command_text.strip()
     if cleaned_name[0] == '@':
         if len(cleaned_name) == 1:
-            return 'You need to give me an actual username'
+            return construct_json(text='You need to give me an actual username')
         cleaned_name = cleaned_name[1:]
 
     if re.match(username_regex, cleaned_name):
@@ -69,18 +111,20 @@ def add_privileged_user(user, command_text):
         logger.info('User %s adding user %s to the privileged user group' % 
             (user, cleaned_name))
         
-        if cleaned_name in privileged_users:
-            return '%s already in the privileged user group.' % cleaned_name
+        if cleaned_name in privileged_users():
+            return construct_json(text='%s already in the privileged user group.' % cleaned_name)
 
-        table.put_item(Item = {
+        users_table.put_item(Item = {
             'username': cleaned_name
         })
 
-        return 'Successfully added user %s to the privileged user group' % cleaned_name
+        return construct_json(text='Successfully added user %s to the privileged user group' 
+            % cleaned_name)
     else:
-        logger.warn('User %s attempted to add invalid user %s to the privileged user group' %
-            (user, cleaned_name))
-        return 'Invalid username %s. If this looks like an error, contact a programmer.' % cleaned_name
+        logger.warn('User %s attempted to add invalid user %s to the privileged user group' 
+            % (user, cleaned_name))
+        return construct_json(text=('Invalid username %s. If this looks like an error, '
+            'contact a programmer.') % cleaned_name)
 
 
 def remove_privileged_user(user, command_text):
@@ -89,16 +133,16 @@ def remove_privileged_user(user, command_text):
 
     Returns an error message if not successful, otherwise a success message
     '''
-    if user not in privileged_users:
+    if user not in privileged_users():
         logger.warn('Unprivileged user %s ran /removeuser on %s' % 
                 (user, command_text))
-        return 'You are not a privileged user. This attempt has been logged.'
+        return construct_json(text='You are not a privileged user. This attempt has been logged.')
         ## TODO: log this attempt to DynamoDB, potentially use SNS to notify officers?
 
     cleaned_name = command_text.strip()
     if cleaned_name[0] == '@':
         if len(cleaned_name) == 1:
-            return 'You need to give me an actual username'
+            return construct_json(text='You need to give me an actual username')
         cleaned_name = cleaned_name[1:]
 
     if re.match(username_regex, cleaned_name):
@@ -106,18 +150,20 @@ def remove_privileged_user(user, command_text):
         logger.info('User %s removing user %s from the privileged user group' % 
             (user, cleaned_name))
 
-        if cleaned_name not in privileged_users:
-            return '%s not in the privileged user group.' % cleaned_name
+        if cleaned_name not in privileged_users():
+            return construct_json(text='%s not in the privileged user group.' % cleaned_name)
         
-        table.delete_item(Key = {
+        users_table.delete_item(Key = {
             'username': cleaned_name
         })
         
-        return 'Successfully removed user %s from the privileged user group' % cleaned_name
+        return construct_json(text='Successfully removed user %s from the privileged user group' 
+            % cleaned_name)
     else:
         logger.warn('User %s attempted to remove invalid user %s from the privileged user group' %
             (user, cleaned_name))
-        return 'Invalid username %s. If this looks like an error, contact a programmer.' % cleaned_name
+        return construct_json(text=('Invalid username %s. If this looks like an error, '
+            'contact a programmer.') % cleaned_name)
 
 
 def lambda_handler(event, context):
@@ -130,17 +176,15 @@ def lambda_handler(event, context):
 
     logger.info(event)
     req_body = event['body']
-    #logger.info('request body = %s' % req_body)
     params = parse_qs(req_body)
     token = params['token'][0]
+
     if token not in expected_tokens:
         logger.error('Request token (%s) does not match expected', token)
         raise Exception('Invalid request token')
 
-
     user = params['user_name'][0]
     command = params['command'][0]
-    channel = params['channel_name'][0]
     command_text = params['text'][0]
 
-    return json.loads('%s'%commands[command](user, command_text, post_channels[0]))
+    return json.loads(commands[command](user, command_text))
--- make_zip.sh
+++ make_zip.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Zip all the Python files along with the license file
-zip Watney *.py LICENSE
+pip install requests -t ./
+zip -r Watney *.py LICENSE requests/
+rm -rf ./requests*/
